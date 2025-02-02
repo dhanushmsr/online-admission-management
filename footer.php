18d9c72f801ddc59a67f7078b0a4bfb96f247be0
Dhanush <161998204+dhanushmsr@users.noreply.github.com>
2025-02-02 21:38:27 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+<style type="text/css">
+<!--
+.style1 {
+	color: #000000;
+	font-weight: bold;
+}
+-->
+</style><center>
+ <div class="footer">
+          <div class="style1"> <p> All Rights Reserved. &copy; 2021  Designed By : Dhanush  M, M/2004/CSE/040 , Hindushthan ,coimbatore, TamilNadu  </p>
+</div>
+</div></center>
---
+++ form.php
@@ -0,0 +1,19 @@
+						
+						
+						<form role="form" class="form-horizontal" method= "POST" action="">
+							<div class="form-group">
+								<div class="col-sm-12">
+									<input class="form-control" id="email1" placeholder="Enter your Mobile" type="number" name="txtpin" required>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-12">
+									<input class="form-control" id="exampleInputPassword1" placeholder="password" type="password" name="txtserial" required>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-10">
+									<button type="submit" name="btnsubmit" class="btn btn-light btn-radius btn-brd grd1">Continue >></button>
+								</div>
+							</div>
+						</form>
